c76c3975cd2c26e3ca93bef50275e9027517d847
Jennifer Byrne <jennifer.lindsey.byrne@gmail.com>
2015-02-16 10:51:46 -0600

    copied repository with different name

--- db/migrate/20150216164528_add_color_and_number_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddColorAndNumberToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :color, :null => false
-  	add_column :users, :number, :null => false, :limit => 2
-  end
-end
